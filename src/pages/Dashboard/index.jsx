3e20060c334f736af7670f123899c6d530aef535
NicholasNeto <nicholasneto@outlook.com>
2021-03-25 20:02:17 -0300

    change for ts component Header

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 
-import Header from '../../components/Header';
+
 import api from '../../services/api';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
 
+import {Header} from '../../components/Header';
 import { Food } from '../../components/Food';
 
 
