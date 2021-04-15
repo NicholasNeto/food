216719e3dab77bbf2fae5f6a7af446b1f9294f90
NicholasNeto <nicholasneto@outlook.com>
2021-04-15 00:12:29 -0300

    removed coments not used

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,8 +7,6 @@ import { ModalAddFood } from '../../components/ModalAddFood';
 import { Header } from '../../components/Header';
 import { Food } from '../../components/Food';
 
-
-
 interface FoodType {
   id: number;
   name: string;
@@ -124,97 +122,3 @@ export default function Dashboard() {
     </>
   )
 }
-
-
-// class Dashboard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       foods: [],
-//       editingFood: {},
-//       modalOpen: false,
-//       editModalOpen: false,
-//     }
-//   }
-
-
-
-//   handleUpdateFood = async food => {
-//     const { foods, editingFood } = this.state;
-
-//     try {
-//       const foodUpdated = await api.put(
-//         `/foods/${editingFood.id}`,
-//         { ...editingFood, ...food },
-//       );
-
-//       const foodsUpdated = foods.map(f =>
-//         f.id !== foodUpdated.data.id ? f : foodUpdated.data,
-//       );
-
-//       this.setState({ foods: foodsUpdated });
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-//   handleDeleteFood = async id => {
-//     const { foods } = this.state;
-
-//     await api.delete(`/foods/${id}`);
-
-//     const foodsFiltered = foods.filter(food => food.id !== id);
-
-//     this.setState({ foods: foodsFiltered });
-//   }
-
-//   toggleModal = () => {
-//     const { modalOpen } = this.state;
-
-//     this.setState({ modalOpen: !modalOpen });
-//   }
-
-//   toggleEditModal = () => {
-//     const { editModalOpen } = this.state;
-
-//     this.setState({ editModalOpen: !editModalOpen });
-//   }
-
-//   handleEditFood = food => {
-//     this.setState({ editingFood: food, editModalOpen: true });
-//   }
-
-//   render() {
-//     const { modalOpen, editModalOpen, editingFood, foods } = this.state;
-
-//     return (
-//       <>
-//         <Header openModal={this.toggleModal} />
-//         <ModalAddFood
-//           isOpen={modalOpen}
-//           setIsOpen={this.toggleModal}
-//           handleAddFood={this.handleAddFood}
-//         />
-//         <ModalEditFood
-//           isOpen={editModalOpen}
-//           setIsOpen={this.toggleEditModal}
-//           editingFood={editingFood}
-//           handleUpdateFood={this.handleUpdateFood}
-//         />
-
-//         <FoodsContainer data-testid="foods-list">
-//           {foods &&
-//             foods.map(food => (
-//               <Food
-//                 key={food.id}
-//                 food={food}
-//                 onHandleDelete={this.handleDeleteFood}
-//                 onHandleEditFood={this.handleEditFood}
-//               />
-//             ))}
-//         </FoodsContainer>
-//       </>
-//     );
-//   }
-// };
-
